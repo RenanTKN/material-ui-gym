35190bfc6a15f7f65720ba30f77352d0e37b14f3
RenanTKN <renanwds@gmail.com>
2020-03-13 11:19:42 -0300

    Apply JSPrettier

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -3,12 +3,12 @@ import { AppBar, Toolbar, Typography } from "@material-ui/core/";
 import CreateDialog from "../Exercises/Dialog";
 
 export default ({ muscles, onExerciseCreate }) => (
-	<AppBar position="static">
-		<Toolbar>
-			<Typography variant="body1" color="inherit" style={{ flex: 1 }}>
-				Exercise Database
-			</Typography>
-			<CreateDialog muscles={muscles} onCreate={onExerciseCreate} />
-		</Toolbar>
-	</AppBar>
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="body1" color="inherit" style={{ flex: 1 }}>
+        Exercise Database
+      </Typography>
+      <CreateDialog muscles={muscles} onCreate={onExerciseCreate} />
+    </Toolbar>
+  </AppBar>
 );
