ce1741319becf15d876fc1d391b616c5192c27e8
RenanTKN <renanwds@gmail.com>
2020-04-08 16:47:06 -0300

    Updates

---
+++ precache-manifest.2fde017ae072c5eb62eea35522a64f1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a293d2986d394876b7d45e75d560c4b2",
+    "url": "/material-ui/index.html"
+  },
+  {
+    "revision": "ae503ed55270a7dc4799",
+    "url": "/material-ui/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d505216ce1a04b7a9363",
+    "url": "/material-ui/static/js/2.b7696374.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/material-ui/static/js/2.b7696374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae503ed55270a7dc4799",
+    "url": "/material-ui/static/js/main.f3237dd1.chunk.js"
+  },
+  {
+    "revision": "582e1ff7c176b025d5b8",
+    "url": "/material-ui/static/js/runtime-main.f69ebac8.js"
+  }
+]);
